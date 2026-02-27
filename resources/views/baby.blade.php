d0ad898d03c66420d5aa855bfc2266c55d7ae398
Muhammad Pearl Ocshada <erilleo23@gmail.com>
2026-02-28 04:09:08 +0700

    POS project JS02

---
+++ resources/views/baby.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>Produk Bayi dan Anak</h1>
+    </body>
+</html>
---
+++ resources/views/sales.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Halaman Penjualan</h1>
+        <p>Ini adalah halaman penjualan.</p>
+    </body>
+</html>
