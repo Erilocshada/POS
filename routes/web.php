eb7ee86d00912bf00896d7a16e52bacb2652a495
Muhammad Pearl Ocshada <erilleo23@gmail.com>
2026-03-07 23:03:44 +0700

    Praktikum5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
