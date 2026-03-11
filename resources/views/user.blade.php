b5d7bbe5c7600916e96acfde6bf505a4f3535543
Muhammad Pearl Ocshada <erilleo23@gmail.com>
2026-03-11 22:45:04 +0700

    Praktikum2.3

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,16 +5,10 @@
     <body>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level pengguna</th>
+                <th>Jumlah User</th>
             </tr>
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
+                <td>{{ $data }}</td>
             </tr>
         </table>
     </body>
